a62449acc9fb6001440def9ee3b07d1385aa8bd7
Tony D'Addeo <tony@askhr.com>
2019-08-18 16:53:26 -0500

    Updates

---
+++ precache-manifest.9f259524d32d4e3e9fa71cda037c8fc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e57e5bbf8e7d31fbfd31b9dc75e9fe",
+    "url": "/simple-giphy-search/index.html"
+  },
+  {
+    "revision": "0622d3172740033a25f0",
+    "url": "/simple-giphy-search/static/css/main.51071f6d.chunk.css"
+  },
+  {
+    "revision": "ed3a10030d481aefdac7",
+    "url": "/simple-giphy-search/static/js/2.78189cac.chunk.js"
+  },
+  {
+    "revision": "0622d3172740033a25f0",
+    "url": "/simple-giphy-search/static/js/main.2d781aaf.chunk.js"
+  },
+  {
+    "revision": "c7f56a74908c37fe900c",
+    "url": "/simple-giphy-search/static/js/runtime~main.7225e371.js"
+  }
+]);
